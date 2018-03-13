040f8059554791876d9d131fb5510e801254ce2a
Dustin Dowell <dustindowell22@gmail.com>
2018-03-13 16:55:48 -0500

    remove checks and vars used once

--- index.js
+++ index.js
@@ -7,17 +7,14 @@ var VOID = ['img', 'input', 'meta', 'br', 'wbr', 'embed', 'area', 'base', 'col',
 module.exports = function h (tag, data, children) {
   var attrs = ''
   for (var attr in data) {
-    var value = data[attr]
-    if (typeof value !== 'string') value = value.toString()
-    attrs += ' ' + attr + '="' + value.replace(DOUBLE_QUOTES, '\\"') + '"'
+    attrs += ' ' + attr + '="' + (data[attr] + '').replace(DOUBLE_QUOTES, '\\"') + '"'
   }
 
   var el = '<' + tag + attrs
 
   if (VOID.indexOf(tag) !== -1 && !children) {
     return el + '/>'
-  } else {
-    var inner = Array.isArray(children) ? children.join('') : children || ''
-    return el + '>' + inner + '</' + tag + '>'
   }
+
+  return el + '>' + (Array.isArray(children) ? children.join('') : children || '') + '</' + tag + '>'
 }
